0fc8d5ae76673a08d5f22fedf03d4bc002ebd667
Evan Pikulski <epikulski@gmail.com>
2024-08-25 20:37:45 -0400

    Set up a basic logging configuration.

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__main__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__main__.py
@@ -4,7 +4,20 @@ cookiecutter.__main__
 Default entrypoint for library when it is called directly.
 ex: `python3 -m {{ cookiecutter.project_name }}`
 """
+import logging
+import os
+
 from {{ cookiecutter.project_name }}.main import entrypoint
 
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG').upper()
+
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=LOG_LEVEL,
+        format='%(asctime)s - %(name)s:%(lineno)d - %(levelname)s - %(message)s - ',
+        handlers=[
+            logging.StreamHandler()  # Log to console
+        ]
+    )
+
     entrypoint()
--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/main.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/main.py
@@ -3,7 +3,11 @@
 
 Contains entrypoints for the library.
 """
+import logging
+
+logger = logging.getLogger(__name__)
 
 def entrypoint():
-    """Default entrypoint for {{cookiecutter.project_name}}"""
+    """Default entrypoint for newproject"""
+    logger.debug("Executing {{ cookiecutter.project_name }} entrypoint.")
     print("Welcome to {{ cookiecutter.project_name }}")
